82696c798210ca29a35edc035c5a202494e8e784
Brandon Reppert <breppert@winbuild1.cse.taylor.edu>
2013-11-07 11:43:15 -0500

    Updated automated script files

--- automated_pioneers.sh
+++ automated_pioneers.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+#automated script file for running out pioneers simulations
+#Brandon Reppert, Alex Sjoberg, Phil Brocker, Seungyoung Ji
 
-date >> date.txt #get start time (used to find toatl run time)
-I=0
-while [ $I -le 10 ] #specify the number of times to run the program
+echo "Starting new simulation..." > date.txt
+date >> date.txt #get start time (used to find total run time)
+I=1
+while [ $I -le 1999 ] #specify the number of times (n+1) to run the program
 do
     echo "We are starting a new game :) :) :)"
-    gnome-terminal -x sh -c "pioneers-server-console -c 3 2>> server_dump.txt"  #start pioneers server
+    gnome-terminal -x sh -c "pioneers-server-console -c 2 2>&1 | grep 'won' >> server_dump.txt"  #start pioneers server
     sleep 1
-    ./run.sh 2>> client_dump.txt #start cleints
+    timeout 240 ./run.sh 2>> /dev/null #client_dump.txt (client logging turned off)  #start cleints
     killall pioneers-server-console #kill server when game is over
     I=$(($I+1))
 done
@@ -15,4 +18,3 @@ done
 python python_results.py #run the python parser to parse results
 
 date >> date.txt #get end time 
-
